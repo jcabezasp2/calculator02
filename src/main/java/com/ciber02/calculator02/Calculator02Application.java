9fee2282ea84d3ef10406f9e19860edee3ee6147
javier Cabezas <jcabezasp@live.com>
2024-04-21 19:23:55 +0200

    Añadido esqueleto de Spring Boot

---
+++ src/main/java/com/ciber02/calculator02/Calculator02Application.java
@@ -0,0 +1,13 @@
+package com.ciber02.calculator02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciber02/calculator02/Calculator02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber02.calculator02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
