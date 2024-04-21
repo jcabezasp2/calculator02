cd41ef6d96c5eea42a6401b2af70c36d941bd85d
javier Cabezas <jcabezasp@live.com>
2024-04-21 21:46:01 +0200

    Wip

--- src/main/java/com/ciber02/calculator02/Calculator02Application.java
+++ src/main/java/com/ciber02/calculator02/Calculator02Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Calculator02Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Calculator02Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator02Application.class, args);
+	}
 
 }
